1433ee275a06602f317120404a18456deeb6739d
0xRudeboy <rudeboyweb3@gmail.com>
2025-02-02 19:23:08 +0400

    add Vault and RebaseToken tests + slight refactoring to pre existing functions

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 /**
  * @title RebaseToken
@@ -10,25 +12,31 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @notice The interest rate in the smart contract can only decrease.
  * @notice Each user will have their own interest rate set by the protocols global interest rate at the time of deposit.
  */
-contract RebaseToken is ERC20 {
+contract RebaseToken is ERC20, Ownable, AccessControl {
     error RebaseToken__InterestRateCannotIncrease(uint256 oldInterestRate, uint256 newInterestRate);
 
     uint256 private constant PRECISION_FACTOR = 1e18;
-    uint256 private s_interestRate = 5e10;
+    bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
+
+    uint256 private s_interestRate = (5 * PRECISION_FACTOR) / 1e8; // 10^-8 == 1 / 10^8
     mapping(address user => uint256 interestRate) private s_userInterestRate;
     mapping(address user => uint256 lastUpdatedTimestamp) private s_userLastUpdatedTimestamp;
 
     event InterestRateSet(uint256 newInterestRate);
 
-    constructor() ERC20("RebaseToken", "RBT") {}
+    constructor() ERC20("RebaseToken", "RBT") Ownable(msg.sender) {}
+
+    function grantMintAndBurnRole(address _account) external onlyOwner {
+        _grantRole(MINT_AND_BURN_ROLE, _account);
+    }
 
     /**
      * @notice Set the interest rate for the protocol.
      * @param _newInterestRate The new interest rate for the protocol.
      * @dev The interest rate can only decrease.
      */
-    function setInterestRate(uint256 _newInterestRate) external {
-        if (_newInterestRate > s_interestRate) {
+    function setInterestRate(uint256 _newInterestRate) external onlyOwner {
+        if (_newInterestRate >= s_interestRate) {
             revert RebaseToken__InterestRateCannotIncrease(s_interestRate, _newInterestRate);
         }
         s_interestRate = _newInterestRate;
@@ -40,10 +48,20 @@ contract RebaseToken is ERC20 {
      * @param _to The address of the user to mint the tokens to.
      * @param _amount The amount of tokens to mint.
      */
-    function mint(address _to, uint256 _amount) external {
+    function mint(address _to, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
         _mintAccruedInterest(_to);
         s_userInterestRate[_to] = s_interestRate;
         _mint(_to, _amount);
+    }
+
+    /**
+     * @notice Burn the user tokens when they withdraw from the vault.
+     * @param _from The address of the user to burn the tokens from.
+     * @param _amount The amount of tokens to burn.
+     */
+    function burn(address _from, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
+        _mintAccruedInterest(_from);
+        _burn(_from, _amount);
     }
 
     /**
@@ -57,6 +75,58 @@ contract RebaseToken is ERC20 {
         // 2. multiply the principal balance by the interest that has accumulated in the time since the balance was last updated
 
         return super.balanceOf(_user) * _calculateUserAccumulatedInterestSinceLastUpdate(_user) / PRECISION_FACTOR;
+    }
+
+    /**
+     * @notice Get the principal balance of a user. This is the number of tokens that have currently been minted to the user not including any accrued interest since the last time the user has interacted with the protocol.
+     * @param _user The address of the user to get the principal balance of.
+     * @return The principal balance of the user.
+     */
+    function principalBalanceOf(address _user) public view returns (uint256) {
+        return super.balanceOf(_user);
+    }
+
+    /**
+     * @notice Transfer the user tokens to another user.
+     * @param _recipient The address of the user to transfer the tokens to.
+     * @param _amount The amount of tokens to transfer.
+     * @return bool Whether the transfer was successful.
+     */
+    function transfer(address _recipient, uint256 _amount) public override returns (bool) {
+        _mintAccruedInterest(msg.sender);
+        _mintAccruedInterest(_recipient);
+
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(msg.sender);
+        }
+
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[msg.sender];
+        }
+
+        return super.transfer(_recipient, _amount);
+    }
+
+    /**
+     * @notice Transfer the user tokens from one user to another.
+     * @param _sender The address of the user to transfer the tokens from.
+     * @param _recipient The address of the user to transfer the tokens to.
+     * @param _amount The amount of tokens to transfer.
+     * @return bool Whether the transfer was successful.
+     */
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
+        _mintAccruedInterest(msg.sender);
+        _mintAccruedInterest(_recipient);
+
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(msg.sender);
+        }
+
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[_sender];
+        }
+
+        return super.transferFrom(_sender, _recipient, _amount);
     }
 
     /**
@@ -86,18 +156,25 @@ contract RebaseToken is ERC20 {
         linearInterest = PRECISION_FACTOR + (s_userInterestRate[_user] * timeElapsed);
     }
 
+    /**
+     * @notice Mint the user the accrued interest since the last time they interacted with the protocol (e.g. burn, mint, transfer).
+     * @param _user The address of the user to mint the accrued interest to.
+     */
     function _mintAccruedInterest(address _user) internal {
         // 1. find their current balance of rebase tokens that have been minted to the user -> principal balance
+        uint256 previousPrincipalBalance = super.balanceOf(_user);
+
         // 2. calculate their current balance including any interest -> balanceOf (any rebase tokens minted to them + pending rebase tokens needed to be minted)
+        uint256 currentBalance = balanceOf(_user);
+
         // 3. calculate the amount of rebase tokens to mint -> (2 - 1)
-        // 4. _mint the rebase tokens to the user
-        // 5. set users last updated timestamp
+        uint256 balanceIncrease = currentBalance - previousPrincipalBalance;
 
+        // 4. set users last updated timestamp
         s_userLastUpdatedTimestamp[_user] = block.timestamp;
-    }
 
-    function burn(address _to, uint256 _amount) external {
-        _burn(_to, _amount);
+        // 5. _mint the rebase tokens to the user
+        _mint(_user, balanceIncrease);
     }
 
     /**
@@ -107,5 +184,13 @@ contract RebaseToken is ERC20 {
      */
     function getUserInterestRate(address _user) external view returns (uint256) {
         return s_userInterestRate[_user];
+    }
+
+    /**
+     * @notice Get the current interest rate for the protocol.
+     * @return The interest rate for the protocol at the present time.
+     */
+    function getInterestRate() external view returns (uint256) {
+        return s_interestRate;
     }
 }
---
+++ test/RebaseToken.t.sol
@@ -0,0 +1,197 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {console2} from "forge-std/console2.sol";
+import {RebaseToken} from "src/RebaseToken.sol";
+import {Vault} from "src/Vault.sol";
+import {IRebaseToken} from "src/interfaces/IRebaseToken.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
+
+contract RebaseTokenTest is Test {
+    RebaseToken private rebaseToken;
+    Vault private vault;
+
+    bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
+
+    address public owner = makeAddr("OWNER");
+    address public user = makeAddr("USER");
+
+    function setUp() public {
+        vm.startPrank(owner);
+        rebaseToken = new RebaseToken();
+        vault = new Vault(IRebaseToken(address(rebaseToken)));
+        rebaseToken.grantMintAndBurnRole(address(vault));
+        vm.stopPrank();
+    }
+
+    function addRewardsToVault(uint256 rewardAmount) public {
+        (bool success,) = payable(address(vault)).call{value: rewardAmount}("");
+        require(success, "Failed to send test ETH to vault");
+    }
+
+    function testDepositLinear(uint256 randomFuzzAmount) public {
+        // here we could use vm.assume however this will completely discard the fuzz run if the condition is not met
+        // vm.assume(randomFuzzAmount > 1e5);
+
+        // rather use bounding the randomFuzzAmount to be between 1e5 and type(uint96).max without discarding the fuzz run for max trials
+        randomFuzzAmount = bound(randomFuzzAmount, 1e5, type(uint96).max);
+
+        // 1. deposit
+        vm.startPrank(user);
+        vm.deal(user, randomFuzzAmount);
+        vault.deposit{value: randomFuzzAmount}();
+        vm.stopPrank();
+
+        // 2. check our rebase token balance
+        uint256 startBalance = rebaseToken.balanceOf(user);
+        assertEq(startBalance, randomFuzzAmount);
+        console2.log("startBalance", startBalance);
+
+        // 3. warp the time and check the balance again
+        vm.warp(block.timestamp + 1 hours);
+        uint256 middleBalance = rebaseToken.balanceOf(user);
+        assertGt(middleBalance, startBalance);
+        console2.log("middleBalance", middleBalance);
+
+        // 4. warp the time again by the same amount of internal and check the balance again
+        vm.warp(block.timestamp + 1 hours);
+        uint256 endBalance = rebaseToken.balanceOf(user);
+        assertGt(endBalance, middleBalance);
+        console2.log("endBalance", endBalance);
+
+        // Here the precision factor is creating a 1 wei difference in the balances so we use assertApproxEqAbs to assertEq with a tolerance of 1 wei
+        assertApproxEqAbs(endBalance - middleBalance, middleBalance - startBalance, 1);
+    }
+
+    function testRedeemStraightAway(uint256 randomFuzzAmount) public {
+        randomFuzzAmount = bound(randomFuzzAmount, 1e5, type(uint96).max);
+        vm.deal(user, randomFuzzAmount);
+
+        vm.startPrank(user);
+        vault.deposit{value: randomFuzzAmount}();
+        assertEq(rebaseToken.balanceOf(user), randomFuzzAmount);
+
+        vault.redeem(type(uint256).max);
+        assertEq(rebaseToken.balanceOf(user), 0);
+        assertEq(address(user).balance, randomFuzzAmount);
+        vm.stopPrank();
+    }
+
+    function testRedeemAfterTimePassed(uint256 randomFuzzAmount, uint256 randomFuzzTimePassed) public {
+        randomFuzzAmount = bound(randomFuzzAmount, 1e5, type(uint96).max);
+        randomFuzzTimePassed = bound(randomFuzzTimePassed, 1000, type(uint96).max); // maximum time of 2.5 * 10^21 years!!!
+        vm.deal(user, randomFuzzAmount);
+
+        // 1. deposit funds into the vault
+        vm.prank(user);
+        vault.deposit{value: randomFuzzAmount}();
+        assertEq(rebaseToken.balanceOf(user), randomFuzzAmount);
+
+        // 2. warp the time
+        vm.warp(block.timestamp + randomFuzzTimePassed);
+        uint256 balanceAfterSomeTime = rebaseToken.balanceOf(user);
+
+        // 2. (b) add the rewards to the vault
+        vm.deal(owner, balanceAfterSomeTime - randomFuzzAmount);
+        vm.prank(owner);
+        addRewardsToVault(balanceAfterSomeTime - randomFuzzAmount);
+
+        // 3. redeem the tokens
+        vm.prank(user);
+        vault.redeem(type(uint256).max);
+
+        uint256 ethBalance = address(user).balance;
+        assertEq(ethBalance, balanceAfterSomeTime);
+        assertGt(ethBalance, randomFuzzAmount);
+    }
+
+    function testTransfer(uint256 randomFuzzAmount, uint256 randomFuzzAmountToSend) public {
+        randomFuzzAmount = bound(randomFuzzAmount, 1e5 + 1e5, type(uint96).max);
+        randomFuzzAmountToSend = bound(randomFuzzAmountToSend, 1e5, randomFuzzAmount - 1e5);
+
+        // 1. deposit
+        vm.deal(user, randomFuzzAmount);
+        vm.prank(user);
+        vault.deposit{value: randomFuzzAmount}();
+
+        address user2 = makeAddr("USER2");
+        uint256 userBalance = rebaseToken.balanceOf(user);
+        uint256 user2Balance = rebaseToken.balanceOf(user2);
+        assertEq(userBalance, randomFuzzAmount);
+        assertEq(user2Balance, 0);
+
+        // owner reduces the interest rate
+        vm.prank(owner);
+        rebaseToken.setInterestRate(4e10);
+
+        // 2. transfer
+        vm.prank(user);
+        rebaseToken.transfer(user2, randomFuzzAmountToSend);
+
+        // 3. check the balances
+        uint256 userBalanceAfterTransfer = rebaseToken.balanceOf(user);
+        uint256 user2BalanceAfterTransfer = rebaseToken.balanceOf(user2);
+
+        assertEq(userBalanceAfterTransfer, userBalance - randomFuzzAmountToSend);
+        assertEq(user2BalanceAfterTransfer, randomFuzzAmountToSend);
+
+        // 4. check the interest rate has been inherited (5e10 not 4e10)
+        assertEq(rebaseToken.getUserInterestRate(user), 5e10);
+        assertEq(rebaseToken.getUserInterestRate(user2), 5e10);
+        assertEq(rebaseToken.getInterestRate(), 4e10);
+    }
+
+    function testCannotSetInterestRateIfNotTheOwner(uint256 randomFuzzInterestRate) public {
+        vm.prank(user);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user));
+        rebaseToken.setInterestRate(randomFuzzInterestRate);
+    }
+
+    function testCannotCallMintAndBurnIfRoleNotGranted() public {
+        vm.startPrank(user);
+        vm.expectRevert(
+            abi.encodeWithSelector(IAccessControl.AccessControlUnauthorizedAccount.selector, user, MINT_AND_BURN_ROLE)
+        );
+        rebaseToken.mint(user, 100);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(IAccessControl.AccessControlUnauthorizedAccount.selector, user, MINT_AND_BURN_ROLE)
+        );
+        rebaseToken.burn(user, 100);
+        vm.stopPrank();
+    }
+
+    function testGetPrincipalAmount(uint256 randomFuzzAmount) public {
+        randomFuzzAmount = bound(randomFuzzAmount, 1e5, type(uint96).max);
+
+        vm.deal(user, randomFuzzAmount);
+        vm.prank(user);
+        vault.deposit{value: randomFuzzAmount}();
+        assertEq(rebaseToken.principalBalanceOf(user), randomFuzzAmount);
+
+        vm.warp(block.timestamp + 1 hours);
+        assertEq(rebaseToken.principalBalanceOf(user), randomFuzzAmount);
+    }
+
+    function testGetRebaseTokenAddress() public view {
+        assertEq(vault.getRebaseTokenAddress(), address(rebaseToken));
+    }
+
+    function testInterestRateCanOnlyDecrease(uint256 randomFuzzInterestRate) public {
+        uint256 initialInterestRate = rebaseToken.getInterestRate();
+        randomFuzzInterestRate = bound(randomFuzzInterestRate, initialInterestRate, type(uint256).max);
+
+        vm.prank(owner);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                RebaseToken.RebaseToken__InterestRateCannotIncrease.selector,
+                initialInterestRate,
+                randomFuzzInterestRate
+            )
+        );
+        rebaseToken.setInterestRate(randomFuzzInterestRate);
+        assertEq(rebaseToken.getInterestRate(), initialInterestRate);
+    }
+}
